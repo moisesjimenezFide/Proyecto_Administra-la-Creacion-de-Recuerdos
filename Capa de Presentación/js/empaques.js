681bd9be721da78a6e28776455d5e1f5d641b800
Esteban Ugalde Rojas <77808945+eugalde60459ufideaccr@users.noreply.github.com>
2025-04-09 16:58:52 -0600

    Create empaques.js

---
+++ Capa de Presentación/js/empaques.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const empaques = [
+        { id: 1, nombre: "Caja Cartón", marca: "EcoCaja", proveedor: "Proveedora A", costo: 500, cantidad: 10, unidad: "unidades", costo_por_unidad: 50 },
+        { id: 2, nombre: "TetraPak", marca: "Tetra Inc.", proveedor: "Proveedora B", costo: 1500, cantidad: 50, unidad: "unidades", costo_por_unidad: 30 },
+        { id: 3, nombre: "Bolsas Plásticas", marca: "PlastiPack", proveedor: "Proveedora C", costo: 600, cantidad: 100, unidad: "unidades", costo_por_unidad: 6 },
+        { id: 4, nombre: "Bolsas de Papel", marca: "Papelería XYZ", proveedor: "Proveedora D", costo: 800, cantidad: 150, unidad: "unidades", costo_por_unidad: 5.33 },
+        { id: 5, nombre: "Cajas de Plástico", marca: "Plastibox", proveedor: "Proveedora E", costo: 2200, cantidad: 20, unidad: "unidades", costo_por_unidad: 110 },
+        { id: 6, nombre: "Caja de Aluminio", marca: "AluPack", proveedor: "Proveedora F", costo: 2500, cantidad: 300, unidad: "unidades", costo_por_unidad: 8.33 },
+        { id: 7, nombre: "Botellas PET", marca: "PetPack", proveedor: "Proveedora G", costo: 700, cantidad: 50, unidad: "unidades", costo_por_unidad: 14 },
+        { id: 8, nombre: "Tarro de Vidrio", marca: "VidriPack", proveedor: "Proveedora H", costo: 1500, cantidad: 20, unidad: "unidades", costo_por_unidad: 75 },
+        { id: 9, nombre: "Cartón para Embalaje", marca: "Embalajes SA", proveedor: "Proveedora I", costo: 900, cantidad: 100, unidad: "unidades", costo_por_unidad: 9 },
+        { id: 10, nombre: "Cajitas de Plástico", marca: "Plastibox", proveedor: "Proveedora J", costo: 2000, cantidad: 50, unidad: "unidades", costo_por_unidad: 40 }
+    ];
+});
