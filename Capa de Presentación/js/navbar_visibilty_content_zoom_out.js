c13b0e2952d8348db391e8631fea1c6ed5b09036
Esteban Ugalde Rojas <77808945+eugalde60459ufideaccr@users.noreply.github.com>
2025-05-21 20:31:41 -0600

    mejoras zoom

--- Capa de Presentación/js/navbar_visibilty_content_zoom_out.js
+++ Capa de Presentación/js/navbar_visibilty_content_zoom_out.js
@@ -12,43 +12,33 @@ function adjustNavbarVisibility() {
         navbarNav.style.display = 'flex'; // Mostrar las opciones del menú
         iconElement.style.display = 'none'; // Ocultar el ícono
     }
+
     // Caso 2: Mostrar el botón de colapso si el zoom está entre 25% y 175% y la ventana es de 810px o menos
     else if (zoomLevel >= 25 && zoomLevel <= 175 && windowWidth <= 810) {
         navbarToggler.style.display = 'block'; // Mostrar el botón de colapso
         navbarNav.style.display = 'none'; // Ocultar las opciones del menú
         iconElement.style.display = 'none'; // Ocultar el ícono
     }
+
     // Caso 3: Mostrar el ícono si el zoom está entre 200% y 500% y la ventana está completamente abierta
     else if (zoomLevel >= 200 && zoomLevel <= 500 && windowWidth >= screenWidth) {
         navbarToggler.style.display = 'none'; // Ocultar el botón de colapso
         navbarNav.style.display = 'flex'; // Mostrar las opciones del menú
         iconElement.style.display = 'none'; // Ocultar el ícono
     }
+
     // Caso 4: Mostrar el botón de colapso si el zoom está entre 200% y 500% y la ventana es de 810px o menos
     else if (zoomLevel >= 200 && zoomLevel <= 500 && windowWidth <= 810) {
         navbarToggler.style.display = 'block'; // Mostrar el botón de colapso
         navbarNav.style.display = 'none'; // Ocultar las opciones del menú
         iconElement.style.display = 'block'; // Mostrar el ícono
     }
+
     // Caso 5: Mostrar el ícono si el zoom está fuera del rango 25% a 500%
     else if (zoomLevel < 25 || zoomLevel > 500) {
         navbarToggler.style.display = 'block'; // Mostrar el botón de colapso
         navbarNav.style.display = 'none'; // Ocultar las opciones del menú
         iconElement.style.display = 'block'; // Mostrar el ícono
-    }
-
-    // Mostrar el ícono si:
-    // 1. La ventana está en tamaño completo y el zoom está entre 200% y 500%.
-    // 2. O si la ventana no está en tamaño completo (<= 810 píxeles) y el zoom está entre 100% y 500%.
-    if (
-        (windowWidth >= screenWidth && zoomLevel >= 200 && zoomLevel <= 500) || // Ventana completa y zoom alto
-        (windowWidth <= 810 && zoomLevel >= 100 && zoomLevel <= 500) // Ventana no completa y zoom medio-alto
-        || (zoomLevel < 67 || zoomLevel > 175)
-
-    ) {
-        iconElement.style.display = 'block';
-    } else {
-        iconElement.style.display = 'none';
     }
 }
 window.addEventListener('load', adjustNavbarVisibility);
@@ -68,7 +58,9 @@ function adjustZoom() {
         const adjustedMarginBottom = `${footer.offsetHeight / scale}px`;
         content.style.marginBottom = adjustedMarginBottom;
         content.style.overflow = 'visible';
-    } else if (zoomLevel === 33) {
+    }
+
+    else if (zoomLevel === 33) {
         const scale = 67 / zoomLevel;
         content.style.transform = `scale(${scale})`;
         content.style.transformOrigin = 'top center';
@@ -77,7 +69,9 @@ function adjustZoom() {
         const adjustedMarginBottom = `${footer.offsetHeight / scale}px`;
         content.style.marginBottom = adjustedMarginBottom;
         content.style.overflow = 'visible';
-    } else if (zoomLevel === 25) {
+    }
+
+    else if (zoomLevel === 25) {
         const scale = 67 / zoomLevel;
         content.style.transform = `scale(${scale})`;
         content.style.transformOrigin = 'top center';
@@ -86,7 +80,9 @@ function adjustZoom() {
         const adjustedMarginBottom = `${footer.offsetHeight / scale}px`;
         content.style.marginBottom = adjustedMarginBottom;
         content.style.overflow = 'visible';
-    } else {
+    }
+
+    else {
         content.style.transform = '';
         content.style.transformOrigin = '';
         content.style.width = '100%';
