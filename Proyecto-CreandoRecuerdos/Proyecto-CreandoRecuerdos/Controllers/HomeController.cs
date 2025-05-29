a8dbbad8c4b506726a1d42354b8085cae40fb67a
SebesHJDEV <Sebastianhernandezjimenez019@gmail.com>
2025-05-29 16:46:05 -0600

    Layout

---
+++ Proyecto-CreandoRecuerdos/Proyecto-CreandoRecuerdos/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_CreandoRecuerdos.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+    }
+}
