6ba32015b0d87b3796e2d313f69387cf463ff00d
JR725 <123512232+JR725@users.noreply.github.com>
2025-06-10 00:35:26 -0600

    Commit Jorge
    HU-14 HU-15

---
+++ Proyecto-CreandoRecuerdos/Proyecto-CreandoRecuerdos/Controllers/HorariosController.cs
@@ -0,0 +1,84 @@
+﻿// HorariosController.cs
+using Proyecto_CreandoRecuerdos.base_de_datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Proyecto_CreandoRecuerdos.Controllers
+{
+    public class HorariosController : Controller
+    {
+        [HttpGet]
+        public ActionResult Horarios()
+        {
+            using (var context = new BD_CREANDO_RECUERDOSEntities())
+            {
+                var horarios = context.tabla_horarios.ToList();
+
+                ViewBag.Empleados = context.tabla_usuarios
+                    .Where(u => u.id_rol != 3)
+                    .Select(u => new SelectListItem
+                    {
+                        Value = u.id_usuario.ToString(),
+                        Text = u.nombre
+                    }).ToList();
+
+                return View(horarios);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult GuardarHorario(int id_usuario, string[] dias_semana, TimeSpan hora_entrada, TimeSpan hora_salida)
+        {
+            using (var context = new BD_CREANDO_RECUERDOSEntities())
+            {
+                foreach (var dia in dias_semana)
+                {
+                    var horario = new tabla_horarios
+                    {
+                        id_usuario = id_usuario,
+                        dia_semana = dia,
+                        hora_entrada = hora_entrada,
+                        hora_salida = hora_salida,
+                        activo = true
+                    };
+
+                    context.tabla_horarios.Add(horario);
+                }
+
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+                {
+                    foreach (var validationErrors in ex.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            System.Diagnostics.Debug.WriteLine($"Property: {validationError.PropertyName} Error: {validationError.ErrorMessage}");
+                        }
+                    }
+                    throw;
+                }
+
+                return RedirectToAction("Horarios");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EliminarHorario(int id)
+        {
+            using (var context = new BD_CREANDO_RECUERDOSEntities())
+            {
+                var horario = context.tabla_horarios.Find(id);
+                if (horario == null) return HttpNotFound();
+
+                context.tabla_horarios.Remove(horario);
+                context.SaveChanges();
+                return RedirectToAction("Horarios");
+            }
+        }
+    }
+}
---
+++ Proyecto-CreandoRecuerdos/Proyecto-CreandoRecuerdos/Models/HorarioModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Proyecto_CreandoRecuerdos.Models
+{
+    public class HorarioModel
+    {
+        public int id_horario { get; set; }
+        public int id_usuario { get; set; }
+        public string dia_semana { get; set; }
+        public TimeSpan hora_entrada { get; set; }
+        public TimeSpan hora_salida { get; set; }
+        public string nombre_usuario { get; set; }
+    }
+}
