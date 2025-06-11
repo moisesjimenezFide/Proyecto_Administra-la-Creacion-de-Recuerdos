1230800de26ca3c473d0621a321d1bf7f9265f78
SebesHJDEV <Sebastianhernandezjimenez019@gmail.com>
2025-06-10 23:27:29 -0600

    Gestion de usuarios

--- Proyecto-CreandoRecuerdos/Proyecto-CreandoRecuerdos/Controllers/Registro_UsuariosController.cs
+++ Proyecto-CreandoRecuerdos/Proyecto-CreandoRecuerdos/Controllers/Registro_UsuariosController.cs
@@ -18,7 +18,7 @@ namespace Proyecto_CreandoRecuerdos.Controllers
             return View();
         }
 
-   
+
 
 
 
@@ -40,6 +40,7 @@ namespace Proyecto_CreandoRecuerdos.Controllers
                 return RedirectToAction("registro_usuarios", "Registro_Usuarios");
             }
         }
+
         [HttpPost]
         public ActionResult iniciar_sesion(UsuarioModel model)
         {
@@ -82,6 +83,93 @@ namespace Proyecto_CreandoRecuerdos.Controllers
             Session.Abandon();  // Finaliza la sesión
             return RedirectToAction("registro_usuarios", "Registro_Usuarios");
         }
+
+
+        [HttpGet]
+        public ActionResult gestion_usuarios()
+        {
+            using (var context = new BD_CREANDO_RECUERDOSEntities())
+            {
+                var usuarios = context.sp_obtener_usuarios().ToList();
+
+                // Mapear los datos manualmente
+                var listaUsuarios = usuarios.Select(u => new UsuarioModel
+                {
+                    id = u.id_usuario,
+                    nombre = u.nombre,
+                    id_rol = u.id_rol,
+                    correo = u.correo,
+                    activo = (bool)u.activo,
+                    fecha_creacion = (DateTime)u.fecha_creacion,
+                    contrasenna = u.contrasenna
+                }).ToList();
+
+                return View(listaUsuarios);
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult inactivar_usuarios(int id)
+        {
+            using (var context = new BD_CREANDO_RECUERDOSEntities())
+            {
+                context.sp_inactivar_usuario(id); // Ejecuta el SP con el ID
+            }
+            return RedirectToAction("gestion_usuarios");
+        }
+
+        [HttpPost]
+        public ActionResult activar_usuarios(int id)
+        {
+            using (var context = new BD_CREANDO_RECUERDOSEntities())
+            {
+                context.sp_activar_usuario(id);
+            }
+            return RedirectToAction("gestion_usuarios");
+        }
+
+        [HttpGet]
+        public ActionResult editar_usuario(int id)
+        {
+            using (var context = new BD_CREANDO_RECUERDOSEntities())
+            {
+                var usuario = context.sp_obtener_usuarios().FirstOrDefault(u => u.id_usuario == id);
+                if (usuario == null)
+                    return HttpNotFound();
+
+                // Aquí llamas al procedimiento para obtener roles
+                var roles = context.sp_obtener_roles().ToList();
+
+                var model = new UsuarioModel
+                {
+                    id = usuario.id_usuario,
+                    nombre = usuario.nombre,
+                    correo = usuario.correo,
+                    id_rol = usuario.id_rol,
+                    RolesDisponibles = roles.Select(r => new SelectListItem
+                    {
+                        Value = r.id_rol.ToString(),
+                        Text = r.nombre
+                    }).ToList()
+                };
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult editar_usuario(UsuarioModel model)
+        {
+            using (var context = new BD_CREANDO_RECUERDOSEntities())
+            {
+                int idUsuario = (int)model.id; // convertir long a int
+
+                context.sp_actualizar_usuario(idUsuario, model.nombre, model.id_rol);
+            }
+            return RedirectToAction("gestion_usuarios");
+        }
+
 
 
 
--- Proyecto-CreandoRecuerdos/Proyecto-CreandoRecuerdos/models/UsuarioModel.cs
+++ Proyecto-CreandoRecuerdos/Proyecto-CreandoRecuerdos/models/UsuarioModel.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.Web.Mvc;
 namespace Proyecto_CreandoRecuerdos.Models
 {
     public class UsuarioModel
     {
         public long id { get; set; }
         public string nombre { get; set; }
-        public int id_rol { get; set; } 
+        public int id_rol { get; set; } // 1= admin, 2= trabajador 3= usuario 
         public string correo { get; set; }
         public bool activo { get; set; }
         public DateTime fecha_creacion { get; set; }
         public string contrasenna { get; set; }
+
+        public List<SelectListItem> RolesDisponibles { get; set; }
     }
 }
