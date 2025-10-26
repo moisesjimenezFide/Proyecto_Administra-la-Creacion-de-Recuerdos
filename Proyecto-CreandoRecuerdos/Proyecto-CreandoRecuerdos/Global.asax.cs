9543442251317527b17fc95a8181fdcfc67e1770
eugalde60459ufideaccr <eugalde60459@ufide.ac.cr>
2025-10-26 11:33:58 -0600

    Arreglo Problemas de Seguridad

--- Proyecto-CreandoRecuerdos/Proyecto-CreandoRecuerdos/Global.asax.cs
+++ Proyecto-CreandoRecuerdos/Proyecto-CreandoRecuerdos/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Net;
 
 namespace Proyecto_CreandoRecuerdos
 {
@@ -13,6 +14,10 @@ namespace Proyecto_CreandoRecuerdos
     {
         protected void Application_Start()
         {
+            // Forzar TLS 1.2 y TLS 1.3
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | (SecurityProtocolType)12288;
+            // Nota: TLS 1.3 no está definido en .NET Framework < 4.8, por eso usamos el valor numérico 12288.
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
